77c193465f9909317a90760b049fa9b4b9ccb7a9
Luka Bilic <phoenix-023@hotmail.com>
2018-11-26 22:32:26 +0100

    Sitne promene na modelu

--- src/main/java/com/kits/project/model/Passenger.java
+++ src/main/java/com/kits/project/model/Passenger.java
@@ -12,6 +12,29 @@ public class Passenger {
     @Column(name = "id", updatable = false, nullable = false)
     private Long id;
 
+    @Column(name = "name")
     private String name;
 
+    public Passenger() {
+    }
+
+    public Passenger(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
