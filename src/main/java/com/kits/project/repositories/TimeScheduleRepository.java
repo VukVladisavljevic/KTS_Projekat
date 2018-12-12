67d69dc1f8ac525deeb84fa6e14e6986288a921f
lupus <vuk.vladisavljevic@gmail.com>
2018-12-12 19:08:03 +0100

    Ubačene osnovne funkcionalnosti login/register u servisima

---
+++ src/main/java/com/kits/project/repositories/TimeScheduleRepository.java
@@ -0,0 +1,7 @@
+package com.kits.project.repositories;
+
+import com.kits.project.model.TimeSchedule;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TimeScheduleRepository extends JpaRepository<TimeSchedule, Long> {
+}
--- src/main/java/com/kits/project/services/implementations/StationServiceImplementation.java
+++ src/main/java/com/kits/project/services/implementations/StationServiceImplementation.java
@@ -3,13 +3,27 @@ package com.kits.project.services.implementations;
 
 import com.kits.project.DTOs.StationDTO;
 import com.kits.project.model.Station;
+import com.kits.project.repositories.StationRepository;
 import com.kits.project.services.interfaces.StationServiceInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class StationServiceImplementation implements StationServiceInterface {
+
+    @Autowired
+    private StationRepository stationRepository;
+
     @Override
     public Station addNewStation(StationDTO stationDTO) {
+
+        Station stationExisting = stationRepository.findByName(stationDTO.name);
+
+        if (stationExisting == null) {
+            Station newStation = stationRepository.save(new Station(stationDTO));
+            return newStation;
+        }
+
         return null;
     }
 
