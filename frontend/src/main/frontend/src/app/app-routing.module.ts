c5d8afc90409ac2a223262ac3dd8f0bf386a7467
Luka Bilic <phoenix-023@hotmail.com>
2019-01-16 22:54:25 +0100

    Merge

--- frontend/src/main/frontend/src/app/app-routing.module.ts
+++ frontend/src/main/frontend/src/app/app-routing.module.ts
@@ -12,7 +12,6 @@ const routes: Routes = [
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegistrationComponent },
   { path: 'timetable', component: TimetableComponent },
-  { path: 'register', component: RegistrationComponent },
   { path: 'lines-map', component: LinesMapComponent },
   { path: 'lines', component: LinesComponent }
 ];
